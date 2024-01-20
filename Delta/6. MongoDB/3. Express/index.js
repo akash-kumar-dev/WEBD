f4f1a34b788f1fd37d66057cebf0c9a91607dc0b
fsociety student <fsociety_ak@proton.me>
2024-01-20 18:08:15 +0530

    Error Handling: wrapAsync function, Mongoose Error- casteError, ValidationError

--- Delta/6. MongoDB/3. Express/index.js
+++ Delta/6. MongoDB/3. Express/index.js
@@ -41,23 +41,21 @@ app.get("/chats/new", (req, res) => {
 })
 
 // Create Route
-app.post("/chats", (req, res) => {
-    let { from, to, msg } = req.body;
-    let newChat = new Chat({
-        from: from,
-        to: to,
-        msg: msg,
-        created_at: new Date()
-    });
-    // console.log(newChat);
-    newChat.save()
-        .then((res) => {
-            console.log("new chat saved");
-        })
-        .catch((err) => {
-            console.log(err);
+app.post("/chats", (req, res, next) => {
+    try {
+        let { from, to, msg } = req.body;
+        let newChat = new Chat({
+            from: from,
+            to: to,
+            msg: msg,
+            created_at: new Date()
         });
-    res.redirect("/chats");
+        // console.log(newChat);
+        newChat.save();
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
 });
 
 // edit route
@@ -85,15 +83,53 @@ app.delete("/chats/:id", async (req, res) => {
     res.redirect("/chats");
 });
 
+// // New - show Route
+// app.get("/chats/:id", async (req, res, next) => {
+//     // try and catch here for casting error
+//     try {
+//         let { id } = req.params;
+//         let chat = await Chat.findById(id);
+//         if (!chat) {
+//             // throw new ExpressError(404, "Chat not found");  // in async error Express By default not calling to next
+//             return next(new ExpressError(404, "Chat not found"));
+//             // handling id check error
+//         }
+//         res.render("edit.ejs", { chat });
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
+// wrapAsync function
+function asyncWrap(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    }
+}
+
 // New - show Route
-app.get("/chats/:id", async (req, res, next) => {
+app.get("/chats/:id", asyncWrap(async (req, res, next) => {
     let { id } = req.params;
     let chat = await Chat.findById(id);
-    if(!chat) {
-        // throw new ExpressError(404, "Chat not found");  // in async error Express By default not calling to next
+    if (!chat) {
         return next(new ExpressError(404, "Chat not found"));
     }
     res.render("edit.ejs", { chat });
+}));
+
+// Mongoose Errors - castError, validationError
+const handleValidationErr = (err) => {
+    console.log("this is a Validation Error.");
+    console.dir(err.message);
+    return err;
+}
+
+app.use((err, req, res, next) => {
+    console.log("Error name -", err.name);
+    if(err.name == "ValidationError") {
+        err = handleValidationErr(err);
+    }
+    next(err);
 });
 
 // error handling middleware
