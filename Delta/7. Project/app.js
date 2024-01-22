838c47a682e12f547fd6c0aa23da6bfd5d04f018
fsociety student <fsociety_ak@proton.me>
2024-01-22 10:56:59 +0530

    Project WanderLust: phase-1(c) Validation for schema(Middleware)

--- Delta/7. Project/app.js
+++ Delta/7. Project/app.js
@@ -47,6 +47,16 @@ app.get("/", (req, res) => {
 //     res.send("Data saved");
 // });
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(".");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
 // index route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -66,28 +76,8 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 // create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    let result = listingSchema.validate(req.body);
-    console.log(result);
-    if(result.error) {
-        throw new ExpressError(400, result.error);
-    }
-    // validation for schema
-    // if (!req.body.listing) {
-    //     throw new ExpressError(400, "Send valid data for listing");
-    //     // 400 -> Bad Request
-    // }
-    // let { title, description, image, price, country, location } = req.body;
+app.post("/listings",validateListing , wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
-    // if (!newListing.title) {
-    //     throw new ExpressError(400, "Title is missing!");
-    // }
-    // if (!newListing.description) {
-    //     throw new ExpressError(400, "Description is missing!");
-    // }
-    // if (!newListing.price) {
-    //     throw new ExpressError(400, "Price is missing!");
-    // }
     await newListing.save();
     res.redirect("/listings");
 }));
@@ -100,7 +90,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 // Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id",validateListing ,wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
--- Delta/7. Project/schema.js
+++ Delta/7. Project/schema.js
@@ -3,9 +3,9 @@ const Joi = require("joi");
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
-        description: Joi.string.required(),
-        loaction: Joi.string.required(),
-        country: Joi.string.required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string().allow("", null),
     }).required(),
