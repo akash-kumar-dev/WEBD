7a4738321c2e6750ab653b133c9b5372797fe623
fsociety student <fsociety_ak@proton.me>
2024-01-27 19:07:28 +0530

    Project WanderLust: phase-2(b) routes seprated by Express Router

---
+++ Delta/7. Project/routes/review.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
+// Validate Reviews
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(".");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+// Reviews
+//  Post route
+router.post("/", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+
+}));
+
+// Delete Review route
+router.delete("/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    // pull Operator removes from an existing array all instances of a values that match a specified condition
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}));
+
+module.exports = router;
