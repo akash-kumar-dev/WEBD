c78a350087a886927aa19df38c0b254ca99afa60
fsociety <fsociety_ak@proton.me>
2024-03-05 09:06:47 +0530

    Project WanderLust: phase-2(d) added authentication required plugins

--- Delta/7. Project/app.js
+++ Delta/7. Project/app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -49,12 +52,29 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     // console.log(res.locals.success);
     next();
 });
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student",
+    });
+
+    let registeredUser = await User.register(fakeUser, "pasword");
+    res.send(registeredUser);
+})
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
