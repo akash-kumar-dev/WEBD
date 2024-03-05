65b2e4ed949a7493c19fafc27f7135700b66fb17
fsociety <fsociety_ak@proton.me>
2024-03-05 10:21:03 +0530

    Project WanderLust: phase-2(d) login added

--- Delta/7. Project/routes/user.js
+++ Delta/7. Project/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 
 router.get("/signup", (req, res) => {
@@ -21,5 +22,21 @@ router.post("/signup", wrapAsync(async (req, res) => {
         res.redirect("/signup");
     }
 }));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true,
+    }),
+    async (req, res) => {
+        req.flash("success", "welcome back to WanderLust");
+        res.redirect("/listings");
+    }
+)
 
 module.exports = router;
