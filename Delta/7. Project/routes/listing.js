77a5bb5d6002c916c75d60cbbb76bfcb87f5a5b2
fsociety_student <fsociety_ak@proton.me>
2024-03-09 12:36:16 +0530

    Project WanderLust: phase-3(a) implementation of router.route

--- Delta/7. Project/routes/listing.js
+++ Delta/7. Project/routes/listing.js
@@ -6,17 +6,24 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listing.js");
 
-// index route
-router.get("/", wrapAsync(listingController.index));
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// show route
-router.get("/:id", wrapAsync(listingController.showListing));
-
-// create Route
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        validateListing,
+        wrapAsync(listingController.updateListing))
+    .delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing));
 
 // Edit Route
 router.get("/:id/edit",
@@ -24,17 +31,5 @@ router.get("/:id/edit",
     isOwner,
     wrapAsync(listingController.renderEditForm));
 
-// Update Route
-router.put("/:id",
-    isLoggedIn,
-    isOwner,
-    validateListing,
-    wrapAsync(listingController.updateListing));
-
-// Delete Route
-router.delete("/:id",
-    isLoggedIn,
-    isOwner,
-    wrapAsync(listingController.destroyListing));
 
 module.exports = router;
