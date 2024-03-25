007eb0b20df68d287e87c3c3937c2efab4e70cb9
fsociety student <fsociety_ak@proton.me>
2024-03-25 10:57:14 +0530

    rendering Array, conditionals element/styles

--- Delta/10. React/1-Basic-React-App/src/Product.jsx
+++ Delta/10. React/1-Basic-React-App/src/Product.jsx
@@ -1,11 +1,14 @@
 import "./Product.css"
 
-function Product({ title, price = 1, features, features2 }) {
+function Product({ title, price = 1, features }) {
+    // const list = features.map((feature) => <li>{feature}</li>);
     return (
         <div className="Product">
             <h2>{title}</h2>
-            <p>{features.a}</p>
+            {/* <p>{list}</p> */}
+            <p>{features.map((feature) => <li>{feature}</li>)}</p>
             <h4>Price : {price}</h4>
+            {price > 3000 ? <p>"Discount of 5%"</p> : null}
         </div>
     );
 }
--- Delta/10. React/1-Basic-React-App/src/ProductTab.jsx
+++ Delta/10. React/1-Basic-React-App/src/ProductTab.jsx
@@ -1,19 +1,17 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
-    let options = ["hitech", "durable", "fast"];    // Array
-    // let options2 = { a: "hi-tech", b: "durable", c: "fast" };     //object
+    // let options = [<li>"hi-tech"</li>, <li>"durable"</li>, <li>"fast"</li>];     // pre-rapping Mathod
+    let options = ["hi-tech", "durable", "fast"];
     return (
         <>
             <Product
                 title="phone"
                 price={30000}
-                // features={options}
-                // features={["hitech", "Array"]}   // passing Array
-                features={{a:"hitech", b:"Object"}} //passing Object
+                features={options}
             />
-            <Product title="Laptop" price={50000} />
-            <Product title="pen" />
+            {/* <Product title="Laptop" price={50000} />
+            <Product title="pen" /> */}
         </>
     );
 }
