100fe616ec5de7a9c799fb3cf98d70c3a27c2089
fsociety student <fsociety_ak@proton.me>
2024-03-27 14:56:17 +0530

    useState(), Like Button in React

---
+++ Delta/10. React/3-REACT-STATE-CLASS/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css'
+import Counter from './Counter'
+import LikeButton from './LikeButton';
+
+function App() {
+
+  return (
+    <>
+      <h1>States in React</h1>
+      <Counter />
+      <LikeButton />
+    </>
+  )
+}
+
+export default App
---
+++ Delta/10. React/3-REACT-STATE-CLASS/src/LikeButton.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function LikeButton() {
+    let [isLiked, setIsLiked] = useState(false);
+    let toggleLike = () => {
+        setIsLiked(!isLiked);
+    };
+
+    let likeStyle = {color: "red"};
+
+    return (
+        <div>
+            <p onClick={toggleLike}>
+                {isLiked ? ( <i class="fa-solid fa-heart" style={likeStyle}></i>) : (<i class="fa-regular fa-heart"></i>)}
+            </p>
+        </div>
+    )
+}
